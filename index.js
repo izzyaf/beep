8ed4974c39b98edecc6f103b9ac6c481781ec2f9
Le Minh Pham Hoang <le.minh.pham.hoang@gmail.com>
2019-12-18 15:59:53 +0700

    handle bad word being part of a valid word

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const symbol = [
+    '`', '~', '!', '@',  '#', '$',
+    '%', '^', '&', '*',  '(', ')',
+    '_', '-', '+', '=',  '{', '[',
+    '}', ']', '|', '\\', ';', ':',
+    "'", '"', '<', ',',  '>', '.',
+    '?', '/', ' '
+]
+
 module.exports = (string, dictionary = []) => {
     return dictionary.filter(swearWord => {
         const regexWithMultipleSpaces = swearWord.trim().split('').map((str, index) => {
@@ -12,8 +21,26 @@ module.exports = (string, dictionary = []) => {
             return `(\\s*${str})`
         }).join('')
 
-        const regex = new RegExp(`${regexWithMultipleSpaces}`)
+        const regex = new RegExp(regexWithMultipleSpaces)
 
-        return regex.test(string)
+        const matches = regex.exec(string)
+
+        if (matches !== null) {
+            const matchedWord = matches[0]
+            const startAt = matches.index
+
+            const nearestChar = string[startAt + matchedWord.length]
+
+            // end of string
+            if (!nearestChar) {
+                return true
+            }
+
+            // if the nearest character is a symbol, consider it end of string
+            // otherwise, it is part of a longer word
+            return symbol.includes(nearestChar)
+        }
+
+        return false
     })
 }
